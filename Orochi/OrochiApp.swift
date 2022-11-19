6766802d14d6ac136f3f888e234a4853b45a731a
Douglas Henrique de Souza Pereira <50143403+douglazhs@users.noreply.github.com>
2022-11-19 12:31:03 -0300

    First commit

---
+++ Orochi/OrochiApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrochiApp.swift
+//  Orochi
+//
+//  Created by Douglas Henrique de Souza Pereira on 19/11/22.
+//
+
+import SwiftUI
+
+@main
+struct OrochiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
